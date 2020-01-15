85f316726bb7f090ecd295b8830902870a2d5569
abhiman <abhimanyadav@gmail.com>
2020-01-15 18:40:04 +0800

    Add fixes to Price Alert Bot

--- Scrape.js
+++
@@ -1,7 +0,0 @@
-var url = 'https://www.dukascopy.com/swiss/english/marketwatch/main/';
-
-var page = require('webpage').create();
-page.open(url, function () {
-    console.log(page.content);
-    phantom.exit();
-});
